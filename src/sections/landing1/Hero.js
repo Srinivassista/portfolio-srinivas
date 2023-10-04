a1498b9d2e61e5a1c0bd0a50f4de53cd48a7c4de
Srinivas Sista <45307086+Srinivassista@users.noreply.github.com>
2023-10-04 11:01:50 +0200

    changed text in hero.js

--- src/sections/landing1/Hero.js
+++ src/sections/landing1/Hero.js
@@ -37,9 +37,9 @@ const Hero = () => {
             <Col lg="7" className="mb-5 mb-lg-0 pt-5">
               <Box>
                 <Text variant="tag" className="text-uppercase" color="heading" mb="25px">
-                  Wisaroot Lertthaweedech
+                  Srinivas Sista
                 </Text>
-                <Title mb="0.5rem">Senior Data Scientist</Title>
+                <Title mb="0.5rem">Data Analyst/Data Scientist/Business Analyst</Title>
                 <Text
                   color="text"
                   css={`
@@ -48,8 +48,7 @@ const Hero = () => {
                   `}
                   mb="2.5rem"
                 >
-                  Software Engineer | Python | Machine Learning | Driving data insights and building
-                  scalable solutions <span className="emphasize-text">🚀</span>
+                  Data driven business insights | SQL | Python <span className="emphasize-text">🚀</span>
                 </Text>
                 <Box
                   color="text"
@@ -64,30 +63,10 @@ const Hero = () => {
                   mb="2.5rem"
                 >
                   <p>
-                    I'm a <span className="emphasize-text">👨‍💻</span> technology enthusiast with{' '}
-                    {year - 2005} years of coding experience, including{' '}
-                    <span className="emphasize-text">{year - 2017} years of experience</span>{' '}
-                    specializing in <code>Python</code> <span className="emphasize-text">🐍</span>.
-                    I also have expertise in <code>Machine Learning</code>{' '}
-                    <span className="emphasize-text">🤖</span>, and <code>Data Science</code> for{' '}
-                    <code>Time Series</code> and <code>NLP</code>. I hold a{' '}
-                    <span className="emphasize-text">🎓</span>{' '}
-                    <span className="emphasize-text">M.Sc. in Computer Science</span> from
-                    Chulalongkorn University and possess skills in software and web development,
-                    including technologies such as <code>Javascript</code>, <code>SQL</code>,{' '}
-                    <code>CSS</code>, <code>HTML</code>, <code>Frontend</code>, <code>Backend</code>
-                    , <code>Database</code>, <code>Node.js</code>, <code>React</code>, and{' '}
-                    <code>MongoDB</code>.
+                    I am Groot.
                   </p>
                   <p>
-                    Currently, I'm a{' '}
-                    <span className="emphasize-text">senior data scientist 👨‍🔬</span> at Honest
-                    Technology, where I've developed a <code>Python</code>{' '}
-                    <span className="emphasize-text">🐍</span> framework for ML research that is
-                    composable, highly scalable, and easily deployable to production. In addition,
-                    I've also gained skills in <code>DevOps</code> and <code>CI/CD</code>, working
-                    with modern tooling such as <code>Terraform Cloud</code> to build cloud
-                    infrastructure as code <span className="emphasize-text">🏗️</span>.
+                    Currently, I'm Groot.
                   </p>
                 </Box>
                 <Box className="d-flex flex-column align-items-center text-center">
