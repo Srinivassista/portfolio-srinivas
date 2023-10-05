2575b2e0c1016a7da246c70a9474d4aa663cb136
Srinivas Sista <45307086+Srinivassista@users.noreply.github.com>
2023-10-05 11:42:40 +0200

    again

--- src/pages/ml-research-framework.js
+++ src/pages/ml-research-framework.js
@@ -24,7 +24,7 @@ import { BiDollar } from 'react-icons/bi'
 import { FaTrophy, FaSmileBeam } from 'react-icons/fa'
 import { FcFactory } from 'react-icons/fc'
 import { IoIosRocket } from 'react-icons/io'
-import { SiWpexplorer } from 'react-icons/si'
+import { PiMagnifyingGlassLight } from 'react-icons/pi'
 
 import { TbRobot, TbPlugConnected } from 'react-icons/tb'
 
@@ -143,7 +143,7 @@ const WorkSingle = () => {
                           color: #e8ad11;
                         `}
                       >
-                        <SiWpexplorer/>
+                        <PiMagnifyingGlassLight/>
                       </span>
                     </div>
                   </Col>
