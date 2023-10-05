1a122568c44c4f99b7b508aaccb40da090260267
Srinivas Sista <45307086+Srinivassista@users.noreply.github.com>
2023-10-05 14:34:30 +0200

    again

--- src/pages/ml-research-framework.js
+++ src/pages/ml-research-framework.js
@@ -303,10 +303,10 @@ const WorkSingle = () => {
                 a very low p-value thus establishing that the distributions of age by is_fraud were different.</p>
                 <Row className="justify-content-center align-items-center d-flex mt-5 mb-4">
               <Col lg="6" className="mb-5">
-                <img src={imgAgeTrans} alt="" height="auto" width="300" className="img-fluid w-100"/>
+                <img src={imgAgeTrans} alt="" height="300" width="300" className="img-fluid w-100"/>
                 <Row className="justify-content-center align-items-center d-flex mt-5 mb-4">
               <Col lg="6" className="mb-5">
-                <img src={imgAgeTrans2} alt="" height="auto" width="300" />
+                <img src={imgAgeTrans2} alt="" height="300" width="300" />
               </Col>
               </Row>
               </Col>
