08d8a82aa21ab2537a346a2c7e7c603fd505b5ee
Srinivas Sista <45307086+Srinivassista@users.noreply.github.com>
2023-10-05 10:37:28 +0200

    width

--- src/pages/ml-research-framework.js
+++ src/pages/ml-research-framework.js
@@ -248,7 +248,7 @@ const WorkSingle = () => {
               
               <Row className="justify-content-center align-items-center d-flex mt-5 mb-4">
               <Col lg="6" className="mb-5">
-                <img src={imgTimeTrans} alt="" height="100 width="200/>
+                <img src={imgTimeTrans} alt="" height="200" width="400"/>
               </Col>
               </Row>
               </p>
