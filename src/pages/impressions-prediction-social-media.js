662b23f4b5731747a0e63d64a282e6d2034b2cc3
Srinivas Sista <45307086+Srinivassista@users.noreply.github.com>
2023-10-09 10:47:14 +0200

    try this

--- src/pages/impressions-prediction-social-media.js
+++ src/pages/impressions-prediction-social-media.js
@@ -6,41 +6,16 @@ import { Element } from 'react-scroll'
 
 import { Section, Button, Title, Text, Box } from '../components/Core'
 import Contact from '../sections/landing1/Contact'
-import imgSenFol from '../assets/img/portfolio/senfollowers.png'
-import imgFolCount from '../assets/img/portfolio/folcount.png'
-import imgSenFriend from '../assets/img/portfolio/senfriends.png'
-import imgFrndCount from '../assets/img/portfolio/frcount.png'
-import imgSenTweet from '../assets/img/portfolio/sentweets.png'
-import imgTweetCount from '../assets/img/portfolio/twcount.png'
-import imgTweetDays from '../assets/img/portfolio/tweetdays.png'
-import imgTweetYears from '../assets/img/portfolio/tweetyears.png'
-import imgRetweets from '../assets/img/portfolio/retweets.png'
-import imgMentions from '../assets/img/portfolio/mentions.png'
-import imgInteractions from '../assets/img/portfolio/interaction.png'
-import imgCluster from '../assets/img/portfolio/cluster.png'
-import imgConnGraph from '../assets/img/portfolio/conngraph.png'
-import imgDegPlot from '../assets/img/portfolio/degreeplot.png'
-import imgElbow from '../assets/img/portfolio/elbow.png'
-import imgSenatorInt from '../assets/img/portfolio/senint.png'
-import imgHashtags from '../assets/img/portfolio/hashtags.png'
+
 
 import { device, breakpoints } from '../utils'
 
-
-import { HiArrowNarrowLeft,HiArrowNarrowRight} from 'react-icons/hi'
-import { BiDollar } from 'react-icons/bi'
-import { FaTrophy, FaSmileBeam } from 'react-icons/fa'
-import { FcFactory } from 'react-icons/fc'
-import { IoIosRocket } from 'react-icons/io'
-import { HiEye } from 'react-icons/hi'
-import { BiBong } from 'react-icons/bi'
-import { FaProjectDiagram } from 'react-icons/fa'
-import { HiOutlineBeaker } from 'react-icons/hi'
-import { HiOutlineDocumentText } from 'react-icons/hi'
+import { BiListUl } from 'react-icons/bi'
+import { FaDatabase } from 'react-icons/fa'
+import { FaFileExcel } from 'react-icons/fa'
+import { HiArrowNarrowLeft } from 'react-icons/hi'
 
 
-
-import { TbRobot, TbPlugConnected } from 'react-icons/tb'
 
 const WorkSingle = () => {
   return (
@@ -63,12 +38,169 @@ const WorkSingle = () => {
         <Container>
           <Row className="justify-content-center">
             <Col lg="9">
-              <Text variant="tag">Data Analysis</Text>
+              <Text variant="tag">Predictive Modeling</Text>
               <Title variant="secSm" className="my-4">
-                Prediction of impressions and engagement on a social media post
+              Predicting impresions and engagements on social media posts
               </Title>
             </Col>
           </Row>
+
+          
+          <Row className="justify-content-center mt-5 mb-4">
+            <Col lg="9">
+              <Title variant="cardLg">01. Analysis Objective</Title>
+              <p>
+              The sales team at The Cirqle wanted to better estimate the impressions and engagements expected from social media creators.
+              The Cirqle's quotes to customers and campaign success were dependent on the accuracy of these predictions.  
+              </p>
+            </Col>
+          </Row>
+          <Row className="justify-content-center mt-5 mb-4">
+            <Col lg="9">
+              <Title variant="cardLg">02. Data Sources</Title>
+              <p>
+              The following data sources were used
+              </p>
+            </Col>
+          </Row>
+          
+          <Row
+            className="justify-content-center mt-0 mb-4"
+            css={`
+              .achievements-icon {
+                font-size: 5rem;
+                justify-content: center;
+                align-items: center;
+              }
+
+              .achievements-desc {
+                text-align: center;
+                padding-left: 0px;
+                @media ${device.lg} {
+                  padding-left: 15px;
+                }
+                @media (max-width: 576px) {
+                  padding-left: 15px;
+                }
+              }
+
+              .achievements-card {
+                margin-top: 15px;
+                @media (max-width: ${breakpoints.sm}px) {
+                  padding-left: 0px;
+                  padding-right: 0px;
+                }
+              }
+
+              .achievements-card2 {
+                background: ${({ theme }) => theme.colors.shadow};
+                padding-bottom: 15px;
+                padding-top: 15px;
+                border-radius: 20px;
+              }
+
+              .achievements-icon2 {
+                width: 80px;
+                height: 80px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                padding: 0.5rem;
+                border-radius: 50%;
+                border: none;
+                transition: 0.4s;
+                font-size: 3rem;
+                color: #000;
+                background: ${({ theme }) => theme.colors.light};
+              }
+
+              .achievements-icon2 > span {
+                margin-top: -10px;
+              }
+            `}
+          >
+            <Col lg="4" className="achievements-card">
+              <Col className="col-12 h-100 achievements-card2">
+                <Row>
+                  <Col className="col-12 d-flex achievements-icon" lg="12" sm="3">
+                    <div className="achievements-icon2">
+                      <span
+                        css={`
+                          color: #e8ad11;
+                        `}
+                      >
+                        <FaDatabase/>
+                      </span>
+                    </div>
+                  </Col>
+                  <Col className="col-12 achievements-desc" lg="12" sm="9">
+                    <Title variant="cardSm" className="mt-3">
+                      Database of all social medai posts from registered influencers
+                    </Title>
+                    
+                  </Col>
+                </Row>
+              </Col>
+            </Col>
+            <Col lg="4" className="achievements-card">
+              <Col className="col-12 h-100 achievements-card2">
+                <Row>
+                  <Col className="col-12 d-flex achievements-icon" lg="12" sm="3">
+                    <div className="achievements-icon2">
+                      <span
+                        css={`
+                          color: #7a996d;
+                        `}
+                      >
+                        <FaFileExcel />
+                      </span>
+                    </div>
+                  </Col>
+                  <Col className="col-12 achievements-desc" lg="12" sm="9">
+                    <Title variant="cardSm" className="mt-3">
+                      Collected screenshots of in-app analytics of posts from past campaigns
+                      </Title>
+                  </Col>
+                </Row>
+              </Col>
+            </Col>
+            </Row>
+            
+        
+            <br>
+            </br>
+            <Row className="justify-content-center mt-5 mb-4">
+            <Col lg="9">
+              <Title variant="cardLg">03. Analysis steps and results</Title>
+              <p>To analyse this data the following tools and steps were taken </p>
+            <p>• Data was cleaned to remove unwanted variables ( more than 1000) in <code>R</code> and <code>Excel</code> </p>
+            <p>• Data was collated using <code>SQL</code></p>
+            <p>• A Naive Bayes model was created used <code>BayesiaLab</code></p>
+            <p>• A report was created to present findings</p>
+            <br>
+            </br>
+            The main findings of this analysis were 
+            <p>• Packaging variables like color, promotion text used had a minimal impact on the sales with the actual promotion being the feature with highest importance</p>
+            <p>• Customer's perception of packaging did not impact sales ( Perception variables were not able to explain enough variance)</p>
+            </Col>
+          </Row>
+          <Row className="justify-content-center mt-5 mb-4">
+            <Link to="/">
+              <Button>
+                <HiArrowNarrowLeft
+                  className="mr-4"
+                  css={`
+                    font-size: 1.25rem;
+                  `}
+                />
+                Home
+              </Button>
+            </Link>
+          </Row> 
+              
+            
+
+
           </Container>
       </Section>
       <Contact />
@@ -76,8 +208,8 @@ const WorkSingle = () => {
   )
 }
 export default WorkSingle
-
-
+          
+          
 
 
 
